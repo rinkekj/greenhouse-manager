a9c432bd3b58b5cca94a46d40bc5a5a5224e113f
Kevin <rinkekj@gmail.com>
2021-04-28 16:40:38 -0400

    create testing branch

--- manage.py
+++ manage.py
@@ -13,7 +13,7 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy import desc
 
 from app import create_app, db, dprint
-from app.models import Role, Employee, Contact, Plant, Orders, Sales, WaterLog, Species, Genus, Supplier, Product, Item
+from app.models import Role, Employee, Plant, WaterLog, Species, Genus, Family, Plant
 from config import Config
 
 from datetime import datetime, date, timedelta
@@ -24,13 +24,17 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db, Employee=Employee, Role=Role)
+    return dict(app=app, 
+        db=db, 
+        Employee=Employee, Role=Role, 
+        Plant=Plant, WaterLog=WaterLog, 
+        Species=Species, Genus=Genus, Family=Family
+        )
 
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 manager.add_command('runserver', Server(host="0.0.0.0"))
-
 
 # Zone A: Full shade, high humidity
 # Zone B: Partial shade, high humidity
@@ -68,248 +72,13 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+    
     fakePlant = Plant(living = True)
     db.session.add(fakePlant)
     db.session.commit()
     db.session.delete(fakePlant)
     db.session.execute('SET FOREIGN_KEY_CHECKS=1;')
     db.session.commit()
-
-@manager.command
-def OrderMerchandise( numItems, orderDate, dayNum ):
-    # Simulation
-    # Generate fake order and add to inventory
-    newOrder = Orders()
-    StuffWeSell = [
-        ('Shovel', 14.30, 24.99, 'B'),
-        ('Peat moss - 5L', 4.75, 12.99, 'D'),
-        ('Peat moss - 10L', 6.00, 19.99, 'D'),
-        ('Perlite - 5L', 3.50, 10.99, 'D'),
-        ('Perlite - 10L', 5.00, 16.99, 'D'),
-        ('Hydroton - 10L', 7.31, 14.99, 'D'),
-        ('Vermiculite - 5L', 3.75, 9.99, 'D'),
-        ('Vermiculite - 10L', 5.75, 13.99, 'D'),
-        ('"Premium" dirt - 5L', 0.50, 24.99, 'D'),
-        ('Systemic granules', 7.50, 17.99, 'A'),
-        ('Copper Fungicide', 11.45, 19.99, 'A'),
-        ('Spray bottle', 0.75, 2.99, 'A'),
-        ('Nursery pot - 3in', 0.25, 1.99, 'B'),
-        ('Nursery pot - 6in', 0.35, 2.99, 'B'),
-        ('Nursery pot - 9in', 0.45, 3.99, 'B')
-        ]
-    for item in StuffWeSell:
-        newItem = Product(
-            name = item[0],
-            quantity = randrange(5,10,1),
-            price = item[2],
-            location = item[3],
-        )
-        invoice = Orders(
-            item = newItem.sku,
-            qty = newItem.quantity,
-            price = item[1],
-            date = orderDate,
-            date_received = (orderDate + timedelta(days=2)),
-            supplier = 'TAGda'
-        )
-        invoice.id = newOrder.id
-
-        db.session.add(invoice)
-        db.session.add(newItem)
-        try:
-            db.session.commit()
-        except IntegrityError:
-            dprint('uh oh')
-            db.session.rollback()
-        else:
-            dprint("Day {}: Order[{}] Item[{}]".format(dayNum, invoice.id, invoice.item))
-
-
-
-@manager.command
-def OrderPlants( numPlants, orderDate, dayNum ):
-    # Simulation
-    # Generate fake plant order and add to inventory
-    
-    newOrder = Orders()
-    
-    plants = Plant.generate_fake( numPlants )
-    for i in plants:
-        invoice = Orders(
-            supplier = 'TGKmf',
-            date = orderDate,
-            date_received = orderDate + timedelta(days=2),
-            item = i.sku,
-            price = i.price/randrange(2, 5, 1),
-            qty = i.quantity
-        )
-        invoice.id = newOrder.id
-
-        db.session.add(invoice)
-
-        # Water plants when they arrive
-        updateLog(i.sku, orderDate, dayNum)
-        try:
-            db.session.commit()
-        except IntegrityError:
-            dprint('uh oh')
-            db.session.rollback()
-        else:
-            dprint("Day {}: Order[{}] Item[{}]".format( dayNum, invoice.id, invoice.item))
-
-@manager.command
-def updateLog(plantSKU, waterDate, dayNum):
-    water = WaterLog(
-            plant = plantSKU,
-            water = 1,
-            date = waterDate
-        )
-    f = randrange(0,2,1)
-    if f == 1:
-        water.feed = 1
-        water.notes = "General purpose fertilizer; half-strength"
-    else:
-        water.feed = 0
-    db.session.add(water)
-    try:
-        db.session.commit()
-    except IntegrityError:
-        dprint('uh oh')
-        db.session.rollback()
-    else:
-        dprint("Day {}: LogEntry[{}] Plant[{}]".format(dayNum, water.id, water.plant))
-
-
-@manager.command
-def checkStock(simDay, dayNum):
-    stock = Product.query.all()
-    reOrder = Orders()
-    for i in stock:
-        if i.quantity == 0:
-            newItem = Product(
-                name = i.name,
-                quantity = randrange(5,10,1),
-                price = i.price,
-                location = i.location,
-            )
-            invoice = Orders(
-                item = newItem.sku,
-                qty = newItem.quantity,
-                price = newItem.price / 3,
-                date = simDay,
-                date_received = (simDay + timedelta(days=2)),
-                supplier = 'TAGda'
-            )
-
-            invoice.id = reOrder.id
-            db.session.delete(i)
-            db.session.add(invoice)
-            db.session.add(newItem)
-            try:
-                db.session.commit()
-            except IntegrityError:
-                dprint('uh oh')
-                db.session.rollback()
-            else:
-                dprint("Day {}: Order[{}] Item[{}]".format(dayNum, invoice.id, invoice.item))
-
-@manager.command
-def waterPlants(simDay, dayNum):
-    # Water simulated plants
-    # Group plants by watering requirements
-    
-    simPlants = db.session.query(Plant).filter(Plant.quantity > 0).all()
-    for i in simPlants:
-        lastLog = db.session.query(WaterLog)\
-                            .filter(WaterLog.plant == i.sku)\
-                            .order_by(desc('date')).first()
-        
-        if i.location == 'A':
-            if (simDay - lastLog.date) == timedelta(days=5):
-                updateLog(i.sku, simDay, dayNum)
-        elif i.location == 'B':
-            if (simDay - lastLog.date) == timedelta(days=7):
-                updateLog(i.sku, simDay, dayNum)
-        else: 
-            if (simDay - lastLog.date) >= timedelta(days=10):
-                updateLog(i.sku, simDay, dayNum)
-
-@manager.command
-def add_fake_data():
-    # Generate fake customers
-    Contact.generate_fake(count=50)
-
-    # All simulated customers are returning customers
-    simContacts = db.session.query(Contact.id).all()
-
-    # Simulate 60 days of slow, daily business
-    simDay = date.today() - timedelta(days=60)
-    dayNum = 0
-
-    # Initial inventory
-    OrderPlants(5, (simDay - timedelta(days=2)), dayNum)
-    OrderMerchandise(5, (simDay - timedelta(days=2)), dayNum)
-
-    # Begin simulation
-    while (date.today() - simDay) > timedelta(days=0):
-        waterPlants(simDay, dayNum)
-        checkStock(simDay, dayNum)
-        if (dayNum % 7) == 0:
-            OrderPlants(5, (simDay - timedelta(days=2)), dayNum)
-
-        # Between 2 and 5 customers per day, chosen at random
-        # (It's ok, everything's overpriced)
-        # Customers come in an buy a handful of random items
-        # Sales are generated and added to DB
-        # Inventory updates
-        for i in range(1, randrange(2,5,1)):
-            transactionID = Sales().id
-            numItems = randrange(1,6,1)
-            shopper = choice(simContacts)
-            merch = []
-            #plantsAvailable = Plant.query.all()
-            itemsAvailable = Item.query.all()
-
-            for item in itemsAvailable:
-                merch.append(item)
-
-            alreadyPicked = []
-            for j in range(1, numItems):
-                merchChoice = choice(merch)
-                if merchChoice.sku in alreadyPicked:
-                    pass
-                elif merchChoice.quantity > 0:
-                    multiplier = randrange(1,5,1)
-                    cart = Sales(
-                        id = transactionID,
-                        date = simDay,
-                        customer = shopper,
-                        item = merchChoice.sku,
-                        salePrice = merchChoice.price
-                    )
-                    # Check if we have enough of that item
-                    if multiplier <= merchChoice.quantity:
-                        cart.qty = multiplier
-                    else:
-                        cart.qty = 1
-                    # Create entry in Sales
-                    db.session.add(cart)
-                    # Update quantities in Inventory DB
-                    merchChoice.updateQty( -cart.qty )
-                    # Don't pick the same item twice
-                    alreadyPicked.append(cart.item)
-                    dprint("Day {}: Customer[{}] purchased Item[{}]".format(dayNum, shopper, cart.item))
-            try:
-                db.session.commit()
-            except IntegrityError:
-                dprint('uh oh')
-                db.session.rollback()
-
-        # Advance simulation one day
-        simDay += timedelta(days=1)
-        dayNum += 1
-
-
 
 @manager.command
 def setup_dev():
